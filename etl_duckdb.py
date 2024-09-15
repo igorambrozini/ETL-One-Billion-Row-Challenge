4ab85993c3d622c4c79ffc78b923d897acf0efd5
Igor Ambrozini <igorwebd@gmail.com>
2024-09-15 20:59:27 -0300

    Addicting etl_duckdb

---
+++ etl_duckdb.py
@@ -0,0 +1,22 @@
+import duckdb
+import time
+
+query = """SELECT
+    station,
+    min (temperature) as Min_temperature,
+    avg (temperature) as Mean_temperature,
+    max (temperature) as Max_temperature
+FROM read_csv("data\\measurements.txt", AUTO_DETECT=FALSE, sep=';', columns={'station':VARCHAR, 'temperature': 'DECIMAL(3,1)'})
+
+GROUP BY station
+ORDER BY station"""
+
+def create_duckcb():
+    duckdb.sql(query).show()
+    
+if __name__ == "__main__":
+    import time
+    start_time = time.time()
+    create_duckcb()
+    took = time.time() - start_time
+    print(f"Duckdb Took: {took:.2f} sec")
